3f8320229f08af48c9783d824c836c83ffb2edca
Mery-Melikyan <mery.melikyan1973@gmail.com>
2024-07-23 23:07:01 +0400

    we completed first version of employee project with routing and without API

--- employees/urls.py
+++ employees/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
      path('', views.index, name="index"),
      path('about/', views.about, name="about"),
      path('contact/', views.contact, name="contact"),
+     path('employees/', views.employees, name="employees"),
+     path('employees/<int:employee_id>', 
+          views.single_employee, name="single_employee")
 ]
     
