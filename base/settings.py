338bd4ab9e2a81b8312c02a3fbf17f6cb01d269d
Mery-Melikyan <mery.melikyan1973@gmail.com>
2024-07-27 01:15:56 +0400

    added slider and logo, added static folder and editted base/settings.py

--- base/settings.py
+++ base/settings.py
@@ -101,15 +101,17 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
